d155a2a007722a8c58e9369ea04f0452b54def6a
Amit-cl <amitkumar2182004@gmail.com>
2026-06-06 11:37:15 +0530

    cors-fixed

--- Backend/server.js
+++ Backend/server.js
@@ -13,9 +13,9 @@ const app = express();
 
 app.use(cors({
   origin: [
-    process.env.CLIENT_URL,
     "http://localhost:5173",
-    "http://localhost:5174"
+    "http://localhost:5174",
+    "https://smart-ats-ai-resume-screening.vercel.app"
   ],
   credentials: true
 }));
